31f3fb849738b259c7aa81634fc5cff719ce1abc
Nowtryz <damien.djmb@gmail.com>
2019-03-21 23:28:00 +0100

    :sparkles: add entrypoint

---
+++ docker/php/docker-php-entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+set -e
+
+# first arg is `-f` or `--some-option`
+if [ "${1#-}" != "$1" ]; then
+	set -- php "$@"
+fi
+
+# look if the container need to be first install
+if [ ! "$(ls -A /var/www/html/cafetapi_content)" ]; then
+     cp /var/cafetapi_content_defaults /var/www/html/cafetapi_content
+fi
+
+exec "$@"
