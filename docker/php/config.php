c6d3cd80ed787a6ebd28049a17eb632184c8b2b4
Nowtryz <damien.djmb@gmail.com>
2019-04-08 22:07:39 +0200

    :whale: removed send mail

--- docker/php/config.php
+++ docker/php/config.php
@@ -25,9 +25,9 @@ class Config
     const installer_external = true;
     const installer_url = self::url . 'get/installer.exe';
     const installer_jar_url = self::url . 'get/installer.jar';
-    const email_sender = 'cafet@exemple.com';
-    const email_noreply = 'noreply@exemple.com';
-    const email_contact = 'contact@exemple.com';
+    const email_sender = 'cafet@bde-apps.isty.uvsq.fr';
+    const email_noreply = 'noreply@bde-apps.isty.uvsq.fr';
+    const email_contact = 'contact@bde-apps.isty.uvsq.fr';
     const email_name = 'Cafet\' Essaim';
     // const email_default_subject = '';
     const session_name = '_cafetapp_' . self::organisation . '_session';
