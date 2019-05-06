9d7f771afc8d8f7af554170a9c8722e7ec941b8a
Nowtryz <damien.djmb@gmail.com>
2019-05-07 00:18:39 +0200

    :whale: use ssmtp with env vars

--- docker/php/docker-php-entrypoint.sh
+++ docker/php/docker-php-entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 set -e
 
 # first arg is `-f` or `--some-option`
@@ -6,7 +6,15 @@ if [ "${1#-}" != "$1" ]; then
 	set -- php "$@"
 fi
 
-# look if the container need to be first install
+# copy default content application
 cp -r -T -n /var/cafetapi_content_defaults/. /var/www/html/cafetapi_content
+
+# Config file for sSMTP sendmail
+cat > /etc/ssmtp/ssmtp.conf << EOL
+root=cafet@${SERVER_DOMAIN}
+mailhub=${MAILHUB}
+hostname=${SERVER_DOMAIN}
+FromLineOverride=YES
+EOL
 
 exec "$@"
