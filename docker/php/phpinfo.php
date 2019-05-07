31da149d48daeeed6767593120b043ef5f9df7f5
Nowtryz <damien.djmb@gmail.com>
2019-05-07 04:19:52 +0200

    :whale: fix phpinfo

--- docker/php/phpinfo.php
+++ docker/php/phpinfo.php
@@ -1,6 +1,4 @@
 <?php
-if (file_exists(CONTENT_DIR . 'config.php')) require_once CONTENT_DIR . 'config.php';
-else require_once INCLUDES_DIR . 'default_configurations.php';
-
+require 'cafetapi_load.php';
 if (Config::debug) php_info();
 else (new ErrorPageBuilder(404))->print();
