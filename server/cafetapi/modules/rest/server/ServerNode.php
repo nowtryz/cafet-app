f110141c047a922c27d54125f0d4912baca1101c
Nowtryz <damien.djmb@gmail.com>
2019-06-14 01:08:56 +0200

    :sparkles: add permission map and info to rest api

--- server/cafetapi/modules/rest/server/ServerNode.php
+++ server/cafetapi/modules/rest/server/ServerNode.php
@@ -5,6 +5,9 @@ use cafetapi\modules\rest\Rest;
 use cafetapi\modules\rest\RestNode;
 use cafetapi\modules\rest\RestResponse;
 use cafetapi\modules\rest\errors\ClientError;
+use cafetapi\user\Perm;
+use cafetapi\modules\rest\HttpCodes;
+use cafetapi\config\Config;
 
 /**
  *
@@ -14,6 +17,8 @@ use cafetapi\modules\rest\errors\ClientError;
 class ServerNode implements RestNode
 {
     const USERS = 'users';
+    const PERMISSIONS_MAP ='permissions.map';
+    const INFORMATION = 'information';
 
     /**
      * (non-PHPdoc)
@@ -26,11 +31,30 @@ class ServerNode implements RestNode
         
         
         switch ($dir) {
-            case self::USERS:   return UsersNode::handle($request);
+            case self::USERS:           return UsersNode::handle($request);
+            case self::INFORMATION:     return self::information($request);
+            case self::PERMISSIONS_MAP: return self::permissions_map($request);
             
             case null: return ClientError::forbidden();
             default:   return ClientError::resourceNotFound('Unknown server/' . $dir . ' node');
         }
+    }
+
+    private static function permissions_map($request) : RestResponse
+    {
+        $reflector = new \ReflectionClass(Perm::class);
+        return new RestResponse(200, HttpCodes::HTTP_200, $reflector->getConstants());
+    }
+    
+    private static function information($request) : RestResponse
+    {
+        return new RestResponse(200, HttpCodes::HTTP_200, [
+            'debug' => Config::debug,
+            'production' => Config::production,
+            'organisation' => Config::organisation,
+            'lang' => Config::lang,
+            'session_name' => Config::session_name
+        ]);
     }
 }
 
