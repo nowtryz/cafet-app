f778b066008ef2b28b3d4a2b04493372e0d6f7a7
Nowtryz <damien.djmb@gmail.com>
2019-11-21 21:38:16 +0100

    :recycle: refractor admin navbar links to a functional component

--- web-client/containers/navbars/AdminNavbarLinks.jsx
+++ web-client/containers/navbars/AdminNavbarLinks.jsx
@@ -27,118 +27,110 @@ import UserControlMenu from '../menus/UserControlMenu'
 import Locale from '../Locale'
 import NotificationControlMenu from '../menus/NotificationControlMenu'
 
-class HeaderLinks extends React.Component {
-    static propTypes = {
-        classes: PropTypes.objectOf(PropTypes.string).isRequired,
-        isAdmin: PropTypes.bool.isRequired,
-    }
-
-    render() {
-        const { classes, isAdmin } = this.props
-
-        const searchButton = classNames(classes.top, classes.searchButton)
-
-        return (
-            <div>
-                <CustomInput
-                    formControlProps={{
-                        className: classNames(classes.top, classes.search),
-                    }}
-                    inputProps={{
-                        placeholder: 'Search',
-                        inputProps: {
-                            'aria-label': 'Search',
-                            className: classes.searchInput,
-                        },
-                    }}
-                />
+const HeaderLinks = ({ classes, isAdmin }) => (
+    <div>
+        <CustomInput
+            formControlProps={{
+                className: classNames(classes.top, classes.search),
+            }}
+            inputProps={{
+                placeholder: 'Search',
+                inputProps: {
+                    'aria-label': 'Search',
+                    className: classes.searchInput,
+                },
+            }}
+        />
+        <Button
+            color="white"
+            aria-label="edit"
+            justIcon
+            round
+            className={classNames(classes.top, classes.searchButton)}
+        >
+            <Search
+                className={`${classes.headerLinksSvg} ${classes.searchIcon}`}
+            />
+        </Button>
+        <LangControlMenu
+            classes={classes}
+            buttonProps={{
+                color: 'transparent',
+                'aria-label': 'Person',
+                justIcon: true,
+                className: classes.buttonLink,
+            }}
+        >
+            <Language
+                className={classNames(classes.headerLinksSvg, classes.links)}
+            />
+            <Hidden mdUp implementation="css">
+                <span className={classes.linkText}>
+                    <Locale>Language</Locale>
+                </span>
+            </Hidden>
+        </LangControlMenu>
+        {isAdmin ? (
+            <div className={classes.managerClasses}>
                 <Button
-                    color="white"
-                    aria-label="edit"
+                    color="transparent"
+                    simple
+                    aria-label="Dashboard"
                     justIcon
-                    round
-                    className={searchButton}
+                    className={classNames(classes.fixJustify, classes.buttonLink)}
+                    component={Link}
+                    to="/admin"
                 >
-                    <Search
-                        className={`${classes.headerLinksSvg} ${classes.searchIcon}`}
-                    />
+                    <Build className={classNames(classes.headerLinksSvg, classes.links)}/>
+                    <Hidden mdUp implementation="css">
+                        <span className={classes.linkText}>
+                            <Locale>Admin Panel</Locale>
+                        </span>
+                    </Hidden>
                 </Button>
-                <LangControlMenu
-                    classes={classes}
-                    buttonProps={{
-                        color: 'transparent',
-                        'aria-label': 'Person',
-                        justIcon: true,
-                        className: classes.buttonLink,
-                    }}
-                >
-                    <Language
-                        className={classNames(classes.headerLinksSvg, classes.links)}
-                    />
-                    <Hidden mdUp implementation="css">
-                        <span className={classes.linkText}>
-                            <Locale>Language</Locale>
-                        </span>
-                    </Hidden>
-                </LangControlMenu>
-                {isAdmin ? (
-                    <div className={classes.managerClasses}>
-                        <Button
-                            color="transparent"
-                            simple
-                            aria-label="Dashboard"
-                            justIcon
-                            className={classNames(classes.fixJustify, classes.buttonLink)}
-                            component={Link}
-                            to="/admin"
-                        >
-                            <Build className={classNames(classes.headerLinksSvg, classes.links)} />
-                            <Hidden mdUp implementation="css">
-                                <span className={classes.linkText}>
-                                    <Locale>Admin Panel</Locale>
-                                </span>
-                            </Hidden>
-                        </Button>
-                    </div>
-                ) : null}
-                <div className={classes.managerClasses}>
-                    <Button
-                        color="transparent"
-                        simple
-                        aria-label="Dashboard"
-                        justIcon
-                        className={classNames(classes.fixJustify, classes.buttonLink)}
-                        component={Link}
-                        to="/dashboard"
-                    >
-                        <Dashboard className={classNames(classes.headerLinksSvg, classes.links)} />
-                        <Hidden mdUp implementation="css">
-                            <span className={classes.linkText}>
-                                <Locale>Dashboard</Locale>
-                            </span>
-                        </Hidden>
-                    </Button>
-                </div>
-                <NotificationControlMenu classes={classes} />
-                <UserControlMenu
-                    classes={classes}
-                    buttonProps={{
-                        color: 'transparent',
-                        'aria-label': 'Person',
-                        justIcon: true,
-                        className: classes.buttonLink,
-                    }}
-                >
-                    <Person className={classNames(classes.headerLinksSvg, classes.links)} />
-                    <Hidden mdUp implementation="css">
-                        <span className={classes.linkText}>
-                            <Locale>Profile</Locale>
-                        </span>
-                    </Hidden>
-                </UserControlMenu>
             </div>
-        )
-    }
+        ) : null}
+        <div className={classes.managerClasses}>
+            <Button
+                color="transparent"
+                simple
+                aria-label="Dashboard"
+                justIcon
+                className={classNames(classes.fixJustify, classes.buttonLink)}
+                component={Link}
+                to="/dashboard"
+            >
+                <Dashboard className={classNames(classes.headerLinksSvg, classes.links)}/>
+                <Hidden mdUp implementation="css">
+                    <span className={classes.linkText}>
+                        <Locale>Dashboard</Locale>
+                    </span>
+                </Hidden>
+            </Button>
+        </div>
+        <NotificationControlMenu classes={classes}/>
+        <UserControlMenu
+            classes={classes}
+            buttonProps={{
+                color: 'transparent',
+                'aria-label': 'Person',
+                justIcon: true,
+                className: classes.buttonLink,
+            }}
+        >
+            <Person className={classNames(classes.headerLinksSvg, classes.links)}/>
+            <Hidden mdUp implementation="css">
+                <span className={classes.linkText}>
+                    <Locale>Profile</Locale>
+                </span>
+            </Hidden>
+        </UserControlMenu>
+    </div>
+)
+
+HeaderLinks.propTypes = {
+    classes: PropTypes.objectOf(PropTypes.string).isRequired,
+    isAdmin: PropTypes.bool.isRequired,
 }
 
 const mapStateToProps = (state) => ({
@@ -163,11 +155,14 @@ const style = (theme) => {
     }
 
     // Override material dashboard style, as it uses !important modifiers
-    defaultStyle.popperNav[breakpoint]['& > div'].marginLeft = '1.5rem'
-    defaultStyle.popperNav[breakpoint]['& > div'].marginRight = '1.5rem'
-    defaultStyle.popperNav[breakpoint]['& > div'].marginBottom = '5px !important'
-    defaultStyle.popperNav[breakpoint]['& > div'].backgroundColor = undefined
-    defaultStyle.popperNav[breakpoint]['& > div']['& ul li'] = undefined
+    defaultStyle.popperNav[breakpoint]['& > div'] = {
+        ...defaultStyle.popperNav[breakpoint]['& > div'],
+        marginLeft: '1.5rem',
+        marginRight: '1.5rem',
+        marginBottom: '5px !important',
+        backgroundColor: undefined,
+        '& ul li': undefined,
+    }
 
     return defaultStyle
 }
